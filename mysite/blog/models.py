b7887df4588cbb2c97321a6ac46562da9eaa6c05
roopxx <127219908+roopxx@users.noreply.github.com>
2024-09-30 14:53:03 +0530

    Create a simple blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -16,13 +23,22 @@ class Post(models.Model):
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=False)
+    updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom 
     
     class Meta:
         ordering = ('-publish',)
     
     def __str__(self):
         return self.title
+  
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+        
