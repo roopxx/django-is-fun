12b6f45801aa991b01aa1d86f8f8c1640236a295
roopxx <127219908+roopxx@users.noreply.github.com>
2024-10-01 11:10:17 +0530

    Clean code: Maintain functional views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
-from django.views.generic import ListView
+from .models import Post
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post/list.html'
-    context_object_name = 'posts'
-    paginate_by = 3
-
-    def get_queryset(self):
-        queryset = Post.published.all()
-        tag_slug = self.kwargs.get('tag_slug')
-        if tag_slug:
-            tag = get_object_or_404(Tag, slug=tag_slug)
-            queryset = queryset.filter(tags__in=[tag])
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tag'] = self.kwargs.get('tag_slug')
-        return context
-    
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
+        
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                                    status='published',
